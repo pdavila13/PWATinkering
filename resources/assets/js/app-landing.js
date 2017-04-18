09991600985f1e1a14748a360d61c9520dfd249f
Paolo Dávila <pdavila@iesebre.com>
2017-04-18 20:27:36 +0200

    Use service-worker

--- resources/assets/js/app-landing.js
+++ resources/assets/js/app-landing.js
@@ -23,3 +23,11 @@ const app = new Vue({
   el: '#app'
 })
 
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js').then(function() {
+    console.log('Service worker registered OK!')
+  }).catch(function (err) {
+    console.log('Service worker registering: ' + err)
+  })
+}
+
