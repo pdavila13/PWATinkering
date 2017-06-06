f6698e181b1bd3cd3e2b8a3c0ab2f9c1153778e3
Paolo Dávila <pdavila@iesebre.com>
2017-06-06 11:35:53 +0200

    Sync public

--- webpack.mix.js
+++ webpack.mix.js
@@ -39,7 +39,7 @@ mix.js('resources/assets/js/app.js', 'public/js')
    .copy('node_modules/admin-lte/plugins','public/plugins')
    .copy('node_modules/icheck/skins/square/blue.png','public/css')
    .copy('node_modules/icheck/skins/square/blue@2x.png','public/css')
-   .copy('resources/assets/js/service-worker.js','public/js');
+   .copy('resources/assets/js/service-worker.js','public');
 
 if (mix.config.inProduction) {
   mix.version();
